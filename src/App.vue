a8c4b3d2963eac43187d8ff86a1878542a64ce0c
Saif Ahmed Anik <saif.ahmed.anik.0@gmail.com>
2022-05-03 23:21:58 +0600

    re-init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            activeUser: {
+                name: 'Maximilian Schwarzmüller',
+                description: 'Site owner and admin',
+                role: 'admin',
+            },
+        };
+    },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+    font-family: sans-serif;
+}
+
+body {
+    margin: 0;
 }
 </style>
