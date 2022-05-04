b3d53b1717fc879f3b6ba98286b3e02bc473971c
Saif Ahmed Anik <saif.ahmed.anik.0@gmail.com>
2022-05-04 18:08:09 +0600

    ✨ feat(namespace slot):

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,38 +1,43 @@
 <template>
-  <section>
-    <div>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
-    <p>{{ infoText }}</p>
-  </section>
+<BaseSlot>
+    <template v-slot:header>
+        <div>
+            <h3>{{ fullName }}</h3>
+            <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+        </div>
+    </template>
+    <template v-slot:default>
+        <p>{{ infoText }}</p>
+    </template>
+</BaseSlot>
 </template>
 
 <script>
 import BaseBadge from './BaseBadge.vue';
-
+import BaseSlot from './BaseSlot.vue';
 
 export default {
-  name: 'UserInfo',
-  props: ['fullName', 'infoText', 'role'],
-  components: {
-    BaseBadge
-  }
+    name: 'UserInfo',
+    props: ['fullName', 'infoText', 'role'],
+    components: {
+        BaseSlot,
+        BaseBadge
+    }
 };
 </script>
 
 <style lang="scss" scoped>
 section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
+    margin: 2rem auto;
+    max-width: 30rem;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
 }
 
 section div {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 </style>
