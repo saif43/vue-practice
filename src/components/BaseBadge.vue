e1b8ec858600709653659c3f2560b2b3801e270d
Saif Ahmed Anik <saif.ahmed.anik.0@gmail.com>
2022-05-04 20:46:25 +0600

    slot scss

--- src/components/BaseBadge.vue
+++ src/components/BaseBadge.vue
@@ -17,7 +17,8 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+
 .badge {
   display: inline-block;
   padding: 0.5rem 1rem;
--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,10 +1,8 @@
 <template>
 <BaseSlot>
     <template v-slot:header>
-        <div>
-            <h3>{{ fullName }}</h3>
-            <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-        </div>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
     </template>
     <template v-slot:default>
         <p>{{ infoText }}</p>
@@ -27,17 +25,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-section {
-    margin: 2rem auto;
-    max-width: 30rem;
-    border-radius: 12px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-    padding: 1rem;
-}
 
-section div {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
 </style>
